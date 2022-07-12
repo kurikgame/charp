116b7daa4c09ee66e9af8d235bdec899bc0432f0
Ivan <kurukgame@gmail.com>
2022-07-13 00:13:41 +0700

    третье задание готово

---
+++ example003_dz3/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+  
+if (a % 2 == 0)
+{
+    Console.WriteLine("четное");
+
+}
+else
+{
+    Console.WriteLine("нечетное");
+}   
+    
