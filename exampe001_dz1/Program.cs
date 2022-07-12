d6957531798cf1e54a0ffcfee7fb68348f18f725
Ivan <kurukgame@gmail.com>
2022-07-12 22:05:52 +0700

    dz1

---
+++ exampe001_dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите первое число ");
+
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("первое  число больше второго ");
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("второе число больше первого");
+}
+Console.ReadKey();
